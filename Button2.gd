f85119734b470860226406cecf42e0ddec059079
Caleb Z <minicooldudezuniga@outlook.com>
2024-02-22 14:02:43 -0800

    Options button functional

--- Button2.gd
+++ Button2.gd
@@ -3,7 +3,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://options menu.tscn") # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +13,7 @@ func _process(delta):
 
 func _on_pressed():
 	get_tree().quit()
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index ("Master"), value) # Replace with function body.
