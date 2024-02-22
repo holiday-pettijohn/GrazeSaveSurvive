5e41b1ae7b88f963a192d74449e2d737188300a5
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2024-02-22 14:15:40 -0800

    Fixing Run_Timer.gd

--- Run_Timer.gd
+++ Run_Timer.gd
@@ -12,6 +12,12 @@ func _ready():
 func _process(delta):
 	if (is_stopped()):
 		#Display results screen scene
-		get_tree().change_scene("res://results.tscn")
+		var root = get_tree().root
+		var level = root.get_node("Run")
+		root.remove_child(level)
+		level.call_deferred("free")
+		var next_level_resource = load("res://results.tscn")
+		var next_level = next_level_resource.instantiate()
+		root.add_child(next_level)
 		pass
 	
