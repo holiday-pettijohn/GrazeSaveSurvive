16a75e716076ccbf7f6530123fa713e633cef388
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2024-02-22 14:35:31 -0800

    Add keybind to Upgrades Button

--- Upgrades_Button.gd
+++ Upgrades_Button.gd
@@ -8,9 +8,7 @@ func _ready():
 func _on_upgrades_pressed():
 	get_tree().change_scene_to_file("res://upgrades.tscn")
 
-func _on_start_run_button_pressed():
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if (Input.is_key_pressed(KEY_U)):
+		_on_upgrades_pressed()
