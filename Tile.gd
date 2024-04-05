4c76e160277ddd5b67e65f83b87ed3b8d65d62d1
jacobshirota <jacobshirota@gmail.com>
2024-04-05 15:44:25 -0700

    Split up Tile functions, add collision

--- Tile.gd
+++ Tile.gd
@@ -12,23 +12,27 @@ var SAMPLE_TILE_1 = 0x000205840131
 
 var db : SQLite
 
-var buffs = {}
+var tile_id : int
+var tile_data : PackedByteArray
 var color = Color()
 
 var tile_offset = Vector2i(40, 40)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	db = SQLite.new()
-	db.path = "res://database.sqlite3"
+	pass
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-#working
-func render_tiles(bitmap):
+
+func render_tiles():
+	var bitmap_data = tile_data.slice(-2)
+	bitmap_data.reverse() # was having problems w/o doing this, endianness maybe?
+	var bitmap = bitmap_data.decode_u16(0)
+	
 	for y in 4:
 		for x in 4:
 			if (bitmap & 0x1):
@@ -42,12 +46,18 @@ func render_tiles(bitmap):
 			bitmap = bitmap >> 1
 
 
-func generate_tile(tile_id : int):
+func generate_tile(tile_id_input : int):
 	# Pulls tile stats and sprite from db based on tile_id, renders tile
 	# Pass (tile_id = -1) to select random tile (ie. for enemy death)
 	
-	if (tile_id == -1): # random tile selection
+	db = SQLite.new()
+	db.path = "res://database.sqlite3"
+	
+	if (tile_id_input == -1): # random tile selection
 		tile_id = randi()%32
+		tile_offset = Vector2i(10, 10)
+	else:
+		tile_id = tile_id_input
 	
 	#get tile data from db
 	db.open_db()
@@ -55,23 +65,26 @@ func generate_tile(tile_id : int):
 	var query_string = "SELECT hex(data) FROM item WHERE item_id = ?;"
 	var param_bindings = [tile_id]
 	db.query_with_bindings(query_string, param_bindings)
-	var tile_data = db.query_result[0]["hex(data)"].hex_decode() #store data into PackedByteArray
+	tile_data = db.query_result[0]["hex(data)"].hex_decode() #store data into PackedByteArray
 	db.close_db()
 	
+	color = Color(tile_data[3], tile_data[4], tile_data[5])
 	
-	var bitmap_data = tile_data.slice(-2)
-	bitmap_data.reverse() # was having problems w/o doing this, endianness maybe?
-	var bitmap = bitmap_data.decode_u16(0)
-	
+
+func get_tile_buffs():
 	var buff_types = (tile_data.slice(3, 4).decode_u8(0))
 	var buff_values = tile_data.slice(0, 3)
+	var buffs = {}
 	
 	for x in 8:
 		if (buff_types & 0x1):
 			buffs[x] = int(buff_values[-1] & 0xFF)
 			buff_values = buff_values.slice(0, -1)
 		buff_types = buff_types >> 1
-	
-	# Can add color data to datatype later if desired
-	color = Color(tile_data[3], tile_data[4], tile_data[5])
-	render_tiles(bitmap)
+	return buffs
+
+
+func _on_area_entered(area):
+	area.get_parent().pick_up_item(tile_id)
+	queue_free() #Get ctrl-v'd
+
