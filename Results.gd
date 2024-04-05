9468648cc821ac64d396b8e17d50fdff862ac456
jacobshirota <jacobshirota@gmail.com>
2024-04-05 14:29:30 -0700

    Add "save_items" function
    To be called on player death/run end

--- Results.gd
+++ Results.gd
@@ -1,14 +1,22 @@
 extends Node2D
 
 
-
+var db : SQLite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
-	pass # Replace with function body.
+	db = SQLite.new()
+	db.path = "res://database.sqlite3"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func save_items(player_id : int, picked_up_items : Array):
+	db.open_db()
+	for item_id in picked_up_items:
+		db.insert_row("player_item_inventory", {"player_id": player_id, "item_id" : item_id})
+	db.close_db()
