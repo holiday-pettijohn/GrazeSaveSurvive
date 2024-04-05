ca4d21474d82d24f5d342597898c1cca060fddb6
jacobshirota <jacobshirota@gmail.com>
2024-04-05 14:18:41 -0700

    Integrate database with Tiles

--- Tile.gd
+++ Tile.gd
@@ -10,7 +10,8 @@ var SAMPLE_TILE_1 = 0x000205840131
 # Buff #s: 8 possible buffs, 1-bit indicator for Buff1-3 (in order: 0x01 -> Buff1=Melee Dmg)
 # Buf Vals: 0-255 multiplier to base buff, dependent on Buff #
 
-var bitmap
+var db : SQLite
+
 var buffs = {}
 var color = Color()
 
@@ -18,7 +19,9 @@ var tile_offset = Vector2i(40, 40)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	db = SQLite.new()
+	db.path = "res://database.sqlite3"
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -39,27 +42,36 @@ func render_tiles(bitmap):
 			bitmap = bitmap >> 1
 
 
-func generate_tile(tile_id):
-	if (tile_id == -1):
+func generate_tile(tile_id : int):
+	# Pulls tile stats and sprite from db based on tile_id, renders tile
+	# Pass (tile_id = -1) to select random tile (ie. for enemy death)
+	
+	if (tile_id == -1): # random tile selection
 		tile_id = randi()%32
 	
 	#get tile data from db
-	var tile_data = SAMPLE_TILE_1
+	db.open_db()
+	#db.verbosity_level = 3
+	var query_string = "SELECT hex(data) FROM item WHERE item_id = ?;"
+	var param_bindings = [tile_id]
+	db.query_with_bindings(query_string, param_bindings)
+	var tile_data = db.query_result[0]["hex(data)"].hex_decode() #store data into PackedByteArray
+	db.close_db()
 	
-	bitmap = (tile_data & 0xFFFF)
-	tile_data = tile_data >> 16
 	
-	var buff_types = (tile_data & 0xFF)
-	tile_data = tile_data >> 8
+	var bitmap_data = tile_data.slice(-2)
+	bitmap_data.reverse() # was having problems w/o doing this, endianness maybe?
+	var bitmap = bitmap_data.decode_u16(0)
+	
+	var buff_types = (tile_data.slice(3, 4).decode_u8(0))
+	var buff_values = tile_data.slice(0, 3)
 	
 	for x in 8:
 		if (buff_types & 0x1):
-			buffs[x] = int(tile_data & 0xFF)
-			tile_data = tile_data >> 8
-			#print(x)
-			#print(buffs[x])
+			buffs[x] = int(buff_values[-1] & 0xFF)
+			buff_values = buff_values.slice(0, -1)
 		buff_types = buff_types >> 1
 	
 	# Can add color data to datatype later if desired
-	color = Color(randf(), randf(), randf())
+	color = Color(tile_data[3], tile_data[4], tile_data[5])
 	render_tiles(bitmap)
