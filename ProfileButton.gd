ae468670ca00ea30a8afd936dffb704ea268b2fb
jacobshirota <jacobshirota@gmail.com>
2024-04-09 13:37:13 -0700

    Update 'Menu' scene

--- ProfileButton.gd
+++ ProfileButton.gd
@@ -1,11 +1,16 @@
-extends Control
+extends OptionButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_item_selected(index):
+	db.player_id = index
+	db.refresh()
