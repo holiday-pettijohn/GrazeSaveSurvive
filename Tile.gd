a4d5020a639a51193141a68bd17c01cc3bcd1cb8
jacobshirota <jacobshirota@gmail.com>
2024-04-12 11:59:48 -0700

    Make tiles pickable
    - When generated from ID, can be dragged and dropped
    - New (placeholder) sprite used instead of ColorRect

--- Tile.gd
+++ Tile.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CollisionObject2D
 
 var SAMPLE_TILE_1 = 0x000205840131
 # TILE DATA FORMAT:
@@ -14,7 +14,7 @@ const TILE_SIZE = 10
 
 var tile_id : int = 0
 var tile_data : PackedByteArray
-var color = Color()
+var pickable = false
 
 var tile_offset = Vector2i(TILE_SIZE, TILE_SIZE)
 
@@ -25,27 +25,28 @@ func _ready():
 func _on_ready():
 	tile_data = db.get_item_data([tile_id])[0]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func render_tiles():
 	var bitmap_data = tile_data.slice(-2)
 	bitmap_data.reverse() # was having problems w/o doing this, endianness maybe?
 	var bitmap = bitmap_data.decode_u16(0)
-	color = Color(tile_data[0], tile_data[1], tile_data[2])
 	
 	for y in 4:
 		for x in 4:
 			if (bitmap & 0x1):
-				var tile_rect = ColorRect.new()
+				var tile_rect = Sprite2D.new()
 				tile_rect.position = Vector2(x*tile_offset[0], y*tile_offset[1])
-				tile_rect.color = color
-				tile_rect.custom_minimum_size.x = tile_offset[0]
-				tile_rect.custom_minimum_size.y = tile_offset[1]
+				tile_rect.set_texture($TheTheSprite.texture)
 				tile_rect.visible = true
 				add_child(tile_rect)
+				
+				var tile_grab = CollisionShape2D.new()
+				tile_grab.position = Vector2(x*tile_offset[0], y*tile_offset[1])
+				tile_grab.debug_color = Color(0, 200, 0, 1)
+				tile_grab.shape = RectangleShape2D.new()
+				tile_grab.shape.size = Vector2(tile_offset[0], tile_offset[1])
+				add_child(tile_grab)
+				
 			bitmap = bitmap >> 1
 
 
@@ -58,7 +59,7 @@ func generate_tile(tile_id_input : int):
 		#tile_offset = Vector2i(10, 10)
 	else:
 		tile_id = tile_id_input
-	
+		pickable = true
 	
 
 func get_tile_buffs():
@@ -78,3 +79,25 @@ func _on_area_entered(area):
 	db.add_items([tile_id])
 	queue_free() #Get ctrl-v'd
 
+
+
+var mouse_in = false
+var clicked = false
+var grabbed_offset = Vector2()
+
+func _process(delta):
+	if Input.is_action_pressed("click"):
+		if mouse_in and !clicked:
+			clicked = true
+			grabbed_offset = position - get_global_mouse_position()
+		if clicked and pickable:
+			position = get_global_mouse_position() + grabbed_offset
+	else:
+		clicked = false
+
+func _on_mouse_entered():
+	grabbed_offset = position - get_global_mouse_position()
+	mouse_in = true
+
+func _on_mouse_exited():
+	mouse_in = false
